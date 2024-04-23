428035f889718ac2cadec5c7b5b1357a5093182d
jaimin koladiya <jaimin.koladiya@lucentinnovation.com>
2024-04-23 11:18:50 +0530

    react debouce package testing progress

---
+++ reactDebounce/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import DebouncedSearchBar from 'react-debounced-searchbar';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <DebouncedSearchBar />
+      </div>
+    </>
+  )
+}
+
+export default App
